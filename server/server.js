02d6665944e58a49632800c47c7885a31345fd7a
Josh <zylo.codes@gmail.com>
2023-10-09 16:34:51 -0500

    test dev/prod envs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')))
+
+app.get('/api/ping', (req, res) => {
+  res.json(true)
+})
+
 
 app.listen(PORT, () => console.log(`Server listening @ http://localhost:${PORT}`))
